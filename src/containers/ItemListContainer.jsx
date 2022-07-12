e4f9e09e5fcd9a6e2b0f983b99ffdd18a0e4940e
Agustin Vigliocco <agusvigliocco@icloud.com>
2022-07-12 18:22:06 -0300

    primer commit

---
+++ src/containers/ItemListContainer.jsx
@@ -0,0 +1,11 @@
+const ItemListContainer = () => {
+    return (
+        <div>
+       
+           <p>Item List Container</p> 
+        
+        </div>
+    );
+}
+
+export default ItemListContainer;
