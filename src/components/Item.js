cc93b2ef01b51c989c22ef7ad663a4eb7d6ed028
Agustin Vigliocco <agusvigliocco@icloud.com>
2022-07-13 19:15:42 -0300

    se agrega cards con productos

---
+++ src/components/Item.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import ItemCount from './ItemCount'
+
+const Item = ({ product }) => {
+
+    return (
+        <div className="card m-5 ">
+            <img className="img-thumbnail" src={product.Imagen} alt="Title" />
+            <div className="card-body d-flex flex-column align-items-center">
+                <h4 className="card-title">{product.Modelo}</h4>
+                <p className="card-text">{product.Color}</p>
+                <p className="card-text">{product.Precio}</p>
+                <a name="" id="" class="btn btn-primary" href="#url" role="button">Detalle</a>
+                <div className='m-3'>
+                    <ItemCount />
+                </div>
+
+            </div>
+        </div>
+
+
+
+    )
+}
+
+export default Item
--- src/containers/ItemListContainer.js
+++ src/containers/ItemListContainer.js
@@ -1,17 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import ItemCount from "../components/ItemCount";
-
-
+import products from "../data/MOCK_DATA.json"
+import ItemList from "../components/ItemList"
 
 const ItemListContainer = () => {
-    const onAdd = (parametro) => {
-        alert   ("la cantidad comprada es:" + parametro);
-    }
+
+    const [productList, setProductList] = useState([])
+
+    const onAdd = (parametro) => { alert("la cantidad comprada es:" + parametro) }
+
+    const myPromise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(products);
+        }, 300);
+    });
+    myPromise.then(res => {
+        setProductList(res);
+    })
+
+
+
     return (
-        <div>       
-           <p>Item List Container</p> 
-            <ItemCount initial={0} stock={15} onAdd={onAdd}/>
-        </div>
+        <>
+            <p>Item List Container</p>
+            <ItemCount initial={0} stock={15} onAdd={onAdd} />
+            <div className="d-flex flex-wrap justify-content-evenly">
+                <ItemList items={productList} />
+            </div>
+        </>
     );
 }
 
