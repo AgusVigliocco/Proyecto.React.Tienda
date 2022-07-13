3d4aa4a488aefb2226444a63b34f66fbd965a594
Agustin Vigliocco <agusvigliocco@icloud.com>
2022-07-12 22:29:20 -0300

    se agrega contador

---
+++ src/components/ItemCount.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+
+
+const ItemCount = ({ initial, stock, onAdd }) => {
+  const [count, setCount] = useState(initial);
+
+  const add = () => {
+    if (count < stock) {
+      const aux = count + 1;
+      setCount(aux);
+    }
+  }
+
+  const rest = () => {
+    if (count < stock) {
+      const aux = count - 1;
+      setCount(aux);
+    }
+  }
+
+  return (
+    <div className='d-flex flex-column align-items-center'>
+      <div className='d-flex'>
+        <button type="button" className="btn btn-primary me-3" onClick={add}><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-cart-plus" viewBox="0 0 16 16">
+          <path d="M9 5.5a.5.5 0 0 0-1 0V7H6.5a.5.5 0 0 0 0 1H8v1.5a.5.5 0 0 0 1 0V8h1.5a.5.5 0 0 0 0-1H9V5.5z" />
+          <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zm3.915 10L3.102 4h10.796l-1.313 7h-8.17zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z" />
+        </svg></button>
+        <p className='h3'>{count}</p>
+        <button type="button" className="btn btn-primary ms-3" onClick={rest}><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-cart-dash" viewBox="0 0 16 16">
+          <path d="M6.5 7a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1h-4z" />
+          <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zm3.915 10L3.102 4h10.796l-1.313 7h-8.17zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z" />
+        </svg></button>
+      </div>
+      <button type="button" className="btn btn-primary mt-2" onClick={onAdd}><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-cart-check" viewBox="0 0 16 16">
+        <path d="M11.354 6.354a.5.5 0 0 0-.708-.708L8 8.293 6.854 7.146a.5.5 0 1 0-.708.708l1.5 1.5a.5.5 0 0 0 .708 0l3-3z" />
+        <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zm3.915 10L3.102 4h10.796l-1.313 7h-8.17zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z" />
+      </svg></button>
+    </div>
+  )
+}
+export default ItemCount
--- src/containers/ItemListContainer.jsx
+++ src/containers/ItemListContainer.jsx
@@ -1,9 +1,16 @@
+import React from "react";
+import ItemCount from "../components/ItemCount";
+
+
+
 const ItemListContainer = () => {
+    const onAdd = (parametro) => {
+        alert   ("la cantidad comprada es:" + parametro);
+    }
     return (
-        <div>
-       
+        <div>       
            <p>Item List Container</p> 
-        
+            <ItemCount initial={0} stock={15} onAdd={onAdd}/>
         </div>
     );
 }
