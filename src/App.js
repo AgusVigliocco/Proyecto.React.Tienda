618f039997e64e174f4e483a1a9257a40c6c3f22
Agustin Vigliocco <agusvigliocco@icloud.com>
2022-07-21 19:47:21 -0300

    solucion a rutas

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import './App.css';
 import Navbar from './components/Navbar';
 import ItemListContainer from './containers/ItemListContainer';
 import ItemDetailContainer from './containers/ItemDetailContainer'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 
 
 const App = () => {
   return (
     <>
-      <Navbar />
-      <ItemListContainer />
-      <ItemDetailContainer />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<ItemListContainer />} />
+          <Route path="/category/:id" element={<ItemListContainer />} />
+          <Route path="/item/:id" element={<ItemDetailContainer />} />
+        </Routes >
+      </BrowserRouter>
     </>
   );
 };
--- src/containers/ItemListContainer.js
+++ src/containers/ItemListContainer.js
@@ -1,15 +1,25 @@
 import React, { useState } from "react";
 import products from "../data/MOCK_DATA.json"
 import ItemList from "../components/ItemList"
+import { useParams } from "react-router";
 
 const ItemListContainer = () => {
 
     const [productList, setProductList] = useState([])
+    const { id } = useParams()
 
     const myPromise = new Promise((resolve, reject) => {
         setTimeout(() => {
-            resolve(products);
-        }, 3000);
+            if (id === undefined) {
+                resolve(products)
+                    .then(res => setProductList(res))
+                    .catch(err => console.log(err))
+            } else {
+                resolve(products.filter(products => products.Categoria === id))
+                    .then(res => setProductList(res))
+                    .catch(err => console.log(err))
+            }
+        }, 10);
     });
     myPromise.then(res => {
         setProductList(res);
